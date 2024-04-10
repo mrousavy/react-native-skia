7535a613fefec4bb9744648af6d2f47a95126b7b
Marc Rousavy <me@mrousavy.com>
2024-04-10 16:16:43 +0200

    Update SkiaMetalSurfaceFactory.h

--- package/ios/RNSkia-iOS/SkiaMetalSurfaceFactory.h
+++ package/ios/RNSkia-iOS/SkiaMetalSurfaceFactory.h
@@ -4,20 +4,54 @@
 #pragma clang diagnostic ignored "-Wdocumentation"
 
 #import "include/core/SkCanvas.h"
+#import "include/gpu/GrDirectContext.h"
+#import "include/gpu/GrYUVABackendTextures.h"
 #import <CoreMedia/CMSampleBuffer.h>
 #import <CoreVideo/CVMetalTextureCache.h>
-#import <include/gpu/GrDirectContext.h>
+#import <memory>
 
 #pragma clang diagnostic pop
 
 using SkiaMetalContext = struct SkiaMetalContext {
+  id<MTLDevice> device = nullptr;
   id<MTLCommandQueue> commandQueue = nullptr;
   sk_sp<GrDirectContext> skContext = nullptr;
 };
 
-class ThreadContextHolder {
+class MetalTextureHolder {
 public:
-  static thread_local SkiaMetalContext ThreadSkiaMetalContext;
+  explicit MetalTextureHolder(CVMetalTextureRef metalTexture);
+  ~MetalTextureHolder();
+
+  const GrBackendTexture& getSkiaTexture() const { return _skiaTexture; }
+
+private:
+  CVMetalTextureRef _metalTexture;
+  GrBackendTexture _skiaTexture;
+};
+
+class YUVMetalTexturesHolder {
+public:
+  explicit YUVMetalTexturesHolder(SkYUVAInfo yuvInfo, std::vector<MetalTextureHolder> textures);
+  ~YUVMetalTexturesHolder();
+
+  GrYUVABackendTextures getSkiaTexture();
+
+private:
+  SkYUVAInfo _yuvInfo;
+  std::vector<MetalTextureHolder> _textures;
+};
+
+enum class CVPixelBufferBaseFormat {
+  yuv,
+  rgb
+};
+
+class ThreadContextHolder {
+private:
+  static thread_local std::unique_ptr<SkiaMetalContext> ThreadSkiaMetalContext;
+public:
+  static const std::unique_ptr<SkiaMetalContext>& getThreadSpecificSkiaContext();
 };
 
 class SkiaMetalSurfaceFactory {
@@ -30,8 +64,10 @@ public:
   makeImageFromCMSampleBuffer(CMSampleBufferRef sampleBuffer);
 
 private:
-  static id<MTLDevice> device;
-  static bool
-  createSkiaDirectContextIfNecessary(SkiaMetalContext *threadContext);
   static CVMetalTextureCacheRef getTextureCache();
+
+  static MetalTextureHolder getTextureFromCVPixelBuffer(CVPixelBufferRef pixelBuffer, size_t planeIndex, MTLPixelFormat pixelFormat);
+  static YUVMetalTexturesHolder* getYUVTexturesFromCVPixelBuffer(CVPixelBufferRef pixelBuffer);
+  static CVPixelBufferBaseFormat getCVPixelBufferBaseFormat(CVPixelBufferRef pixelBuffer);
+  static SkYUVAInfo getYUVAInfoForCVPixelBuffer(CVPixelBufferRef pixelBuffer);
 };
